adbef2a7a6d88175cb584b0f446ee8afe6ea4af9
NazaSoto19 <nazarenosotomayor19@gmail.com>
2025-04-23 20:55:39 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import RrhhHome from "./pages/RrhhHome";
+import AdminDashboard from "./pages/AdminDashboard";
+import Home from "./pages/Home";
 
 function App() {
   return (
@@ -8,6 +10,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/rrhh/home" element={<RrhhHome />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
