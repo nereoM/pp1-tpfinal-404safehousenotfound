9ca775de1b3d82c739584289028f7a83dad212e6
Nereo Manganiello <nereomanganiello@gmail.com>
2025-04-28 03:48:54 -0300

    Agrego funciones para guardar el modeloo entrenado y tabla de oferta laboral

--- server/ml/matching_supervised.py
+++ server/ml/matching_supervised.py
@@ -3,25 +3,54 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+import os
+import pickle
+import json
+from models.schemes import Oferta_laboral
 
-"""
-cvs = [
-    "Soy ingeniero en sistemas, manejo Python, Django y Scrum",
-    "Desarrollador frontend especializado en React y Javascript",
-    "Administrador de empresas con experiencia en recursos humanos",
-    "Programador Java con certificación en metodologías ágiles",
-    "Fullstack developer experto en React, NodeJS y Python",
-    "Candidato junior buscando oportunidad en IT",
-    "Ingeniero con experiencia en C++, algoritmos y bases de datos"
-]
-"""
+
+def cargar_modelo_de_oferta(id_oferta):
+    oferta = Oferta_laboral.query.get(id_oferta)
+
+    if not oferta:
+        raise Exception(f"No se encontró la oferta laboral con id {id_oferta}")
+
+    modelo = pickle.loads(oferta.modelo)
+    vectorizador = pickle.loads(oferta.vectorizador)
+    palabras_clave = json.loads(oferta.palabras_clave)
+
+    return modelo, vectorizador, palabras_clave
+
+def serializar_modelo_y_vectorizador(modelo, vectorizador):
+    modelo_bytes = pickle.dumps(modelo)
+    vectorizador_bytes = pickle.dumps(vectorizador)
+    return modelo_bytes, vectorizador_bytes
+
+def serializar_palabras_clave(palabras_clave):
+    return json.dumps(palabras_clave)
+
+def cargar_cvs_para_entrenamiento(ruta_csv=os.path.join("files", "cv_dataset_final.pdf"), solo_texto=True):
+    df = pd.read_csv(ruta_csv)
+
+    if 'cv' not in df.columns or 'label' not in df.columns:
+        raise ValueError("El CSV debe tener las columnas 'cv' y 'label'.")
+
+    cvs = df['cv'].tolist()
+
+    if solo_texto:
+        return cvs
+    else:
+        etiquetas = df['label'].tolist()
+        return cvs, etiquetas
 
 def entrenar_modelo_supervised(corpus_textos, etiquetas):
     vectorizador = TfidfVectorizer()
     X = vectorizador.fit_transform(corpus_textos).toarray()
     modelo = RandomForestClassifier()
     modelo.fit(X, etiquetas)
-    return modelo, vectorizador
+    modelo_bytes, vectorizador_bytes = serializar_modelo_y_vectorizador(modelo, vectorizador)
+    etiquetas_serializadas = serializar_palabras_clave(etiquetas)
+    return modelo_bytes, vectorizador_bytes, etiquetas_serializadas
 
 def evaluar_cv_supervised(cv_texto, palabras_clave, modelo, vectorizador, umbral_similitud=0.6):
     X_nuevo = vectorizador.transform([cv_texto]).toarray()
