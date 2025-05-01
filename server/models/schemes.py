2b8f17b8bcb6015f994dbd2e1ce8012368a11b3c
SantiagoZarate <santiago2001zarate@gmail.com>
2025-05-01 00:27:43 -0300

    arregla un typo en la declarion de la tabla oferta_analista

--- server/models/schemes.py
+++ server/models/schemes.py
@@ -1,11 +1,14 @@
-from sqlalchemy.ext.hybrid import hybrid_property
-from werkzeug.security import generate_password_hash, check_password_hash
-from .extensions import db
-import pickle
 import json
+import pickle
+
+from sqlalchemy.ext.hybrid import hybrid_property
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from .extensions import db
+
 
 class Usuario(db.Model):
-    __tablename__ = 'usuarios'
+    __tablename__ = "usuarios"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(50), nullable=False)
     apellido = db.Column(db.String(50), nullable=False)
@@ -13,85 +16,100 @@ class Usuario(db.Model):
     correo = db.Column(db.String(100), nullable=False, unique=True)
     _contrasena = db.Column(db.String(512), nullable=False)
     confirmado = db.Column(db.Boolean, default=False)
-    id_empresa = db.Column(db.Integer, db.ForeignKey("empresas.id"), nullable=True)  # Relación con Empresa
-
+    id_empresa = db.Column(
+        db.Integer, db.ForeignKey("empresas.id"), nullable=True
+    )  # Relación con Empresa
 
     @hybrid_property
     def contrasena(self):
         return self._contrasena
-    
+
     @contrasena.setter
     def contrasena(self, contrasena):
         self._contrasena = generate_password_hash(contrasena)
 
     def verificar_contrasena(self, password):
         return check_password_hash(self._contrasena, password)
-    
+
     def confirmar_usuario(self):
         self.confirmado = True
         db.session.commit()
 
-    roles = db.relationship("Rol", secondary='usuarios_roles', back_populates="usuarios")
+    roles = db.relationship(
+        "Rol", secondary="usuarios_roles", back_populates="usuarios"
+    )
 
-    
     def tiene_rol(self, rol):
         return bool(
-            Rol.query.join(Rol.usuarios).filter(Usuario.id == self.id, Rol.slug == rol).count() == 1
+            Rol.query.join(Rol.usuarios)
+            .filter(Usuario.id == self.id, Rol.slug == rol)
+            .count()
+            == 1
         )
 
+
 class Rol(db.Model):
-    __tablename__ = 'roles'
+    __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(50), nullable=False, unique=True)
     permisos = db.Column(db.String(200), nullable=False)
     slug = db.Column(db.String(50), nullable=False, unique=True)
 
-    usuarios = db.relationship("Usuario", secondary='usuarios_roles', back_populates="roles")
+    usuarios = db.relationship(
+        "Usuario", secondary="usuarios_roles", back_populates="roles"
+    )
+
 
 class UsuarioRol(db.Model):
-    __tablename__ = 'usuarios_roles'
-    id_usuario = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
-    id_rol = db.Column(db.Integer, db.ForeignKey('roles.id'), primary_key=True)
+    __tablename__ = "usuarios_roles"
+    id_usuario = db.Column(db.Integer, db.ForeignKey("usuarios.id"), primary_key=True)
+    id_rol = db.Column(db.Integer, db.ForeignKey("roles.id"), primary_key=True)
+
 
 class CV(db.Model):
-    __tablename__ = 'cvs'
+    __tablename__ = "cvs"
     id = db.Column(db.Integer, primary_key=True)
-    id_candidato = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    id_candidato = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
     url_cv = db.Column(db.String(255), nullable=False)
     tipo_archivo = db.Column(db.String(50))
     fecha_subida = db.Column(db.DateTime, default=db.func.now())
 
     usuario = db.relationship("Usuario", backref="cvs")
 
+
 class Empresa(db.Model):
-    __tablename__ = 'empresas'
+    __tablename__ = "empresas"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False, unique=True)
     correo = db.Column(db.String(100), nullable=False, unique=True)
-    id_admin_emp = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    id_admin_emp = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
 
-    admin_emp = db.relationship("Usuario", backref="empresa", foreign_keys=[id_admin_emp])
+    admin_emp = db.relationship(
+        "Usuario", backref="empresa", foreign_keys=[id_admin_emp]
+    )
 
     def __init__(self, nombre, id_admin_emp):
         self.nombre = nombre
         self.correo = f"{nombre.lower().replace(' ', '_')}@empresa.com"
         self.id_admin_emp = id_admin_emp
 
+
 class TarjetaCredito(db.Model):
-    __tablename__ = 'tarjetas_credito'
+    __tablename__ = "tarjetas_credito"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     numero_tarjeta = db.Column(db.String(16), nullable=False, unique=True)
     nombre = db.Column(db.String(100), nullable=False)
     tipo = db.Column(db.String(50), nullable=False)  # Ejemplo: Visa, Mastercard
     cvv = db.Column(db.String(4), nullable=False)
-    usuario_id = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
+    usuario_id = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
 
     usuario = db.relationship("Usuario", backref="tarjetas_credito")
 
+
 class Oferta_laboral(db.Model):
-    __tablename__ = 'ofertas_laborales'
+    __tablename__ = "ofertas_laborales"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_empresa = db.Column(db.Integer, db.ForeignKey('empresas.id'), nullable=False)
+    id_empresa = db.Column(db.Integer, db.ForeignKey("empresas.id"), nullable=False)
     nombre = db.Column(db.String(100), nullable=False)
     descripcion = db.Column(db.Text, nullable=False)
     location = db.Column(db.String(100), nullable=False)
@@ -108,21 +126,28 @@ class Oferta_laboral(db.Model):
 
     empresa = db.relationship("Empresa", backref="ofertas_laborales")
 
+
 class Oferta_analista(db.Model):
-    __tablename__ = 'ofertas_analista'
-    id_oferta = db.Column(db.Integer, db.ForeingKey('ofertas_laborales.id'), primary_key=True)
-    id_analista = db.Column(db.Integer, db.ForeignKey('usuarios.id'), primary_key=True)
+    __tablename__ = "ofertas_analista"
+    id_oferta = db.Column(
+        db.Integer, db.ForeignKey("ofertas_laborales.id"), primary_key=True
+    )
+    id_analista = db.Column(db.Integer, db.ForeignKey("usuarios.id"), primary_key=True)
+
 
 class Job_Application(db.Model):
-    __tablename__ = 'job_application'
+    __tablename__ = "job_application"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    id_candidato = db.Column(db.Integer, db.ForeignKey('usuarios.id'), nullable=False)
-    id_oferta = db.Column(db.Integer, db.ForeignKey('ofertas_laborales.id'), nullable=False)
+    id_candidato = db.Column(db.Integer, db.ForeignKey("usuarios.id"), nullable=False)
+    id_oferta = db.Column(
+        db.Integer, db.ForeignKey("ofertas_laborales.id"), nullable=False
+    )
     id_cv = db.Column(db.Integer, db.ForeignKey("cvs.id"), nullable=False)
     is_apto = db.Column(db.Boolean, nullable=False)
     fecha_postulacion = db.Column(db.DateTime, default=db.func.now())
-    
-    candidato = db.relationship('Usuario', backref='cv_files')
+
+    candidato = db.relationship("Usuario", backref="cv_files")
+
 
 class Licencia(db.Model):
     __tablename__ = "licencias"
@@ -132,8 +157,9 @@ class Licencia(db.Model):
     descripcion = db.Column(db.Text, nullable=True)
     fecha_inicio = db.Column(db.DateTime, nullable=True)
     fecha_fin = db.Column(db.DateTime, nullable=True)
-    certificado_url = db.Column(db.String(255), nullable=True) # AL MENOS POR AHORA
+    certificado_url = db.Column(db.String(255), nullable=True)  # AL MENOS POR AHORA
     estado = db.Column(db.String(50), nullable=False)
+
 
 def guardar_modelo_en_oferta(id_oferta, modelo, vectorizador, palabras_clave):
     oferta = Oferta_laboral.query.get(id_oferta)
