9d009722b473844632733c9f6154cafa4a424dc3
SantiagoZarate <santiago2001zarate@gmail.com>
2025-05-01 23:18:07 -0300

    agrega filtrado de ofertas de trabajo por query params

--- server/routes/candidato.py
+++ server/routes/candidato.py
@@ -1,45 +1,57 @@
-from flask import Blueprint, jsonify, request
-from auth.decorators import role_required
-import os
-from werkzeug.utils import secure_filename
-from models.schemes import Usuario, Rol, TarjetaCredito, Empresa, Oferta_laboral
-from models.extensions import db
-from flask_jwt_extended import get_jwt_identity
-from datetime import datetime, timezone
-from models.schemes import CV, Job_Application
-from ml.extraction import predecir_cv
-from ml.extraction import extraer_texto_pdf, extraer_texto_word
-from ml.matching_semantico import dividir_cv_en_partes
-from sklearn.metrics.pairwise import cosine_similarity
-from ml.modelo import modelo_sbert
 import json
+import os
+from datetime import datetime, timezone
+
+from auth.decorators import role_required
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import get_jwt_identity
+from ml.extraction import extraer_texto_pdf, extraer_texto_word, predecir_cv
+from ml.matching_semantico import dividir_cv_en_partes
+from ml.modelo import modelo_sbert
+from models.extensions import db
+from models.schemes import (
+    CV,
+    Empresa,
+    Job_Application,
+    Oferta_laboral,
+    Rol,
+    TarjetaCredito,
+    Usuario,
+)
+from sklearn.metrics.pairwise import cosine_similarity
+from werkzeug.utils import secure_filename
 
 candidato_bp = Blueprint("candidato", __name__)
+
 
 @candidato_bp.route("/candidato-home", methods=["GET"])
 @role_required(["candidato"])
 def candidato_home():
     return jsonify({"message": "Bienvenido al dashboard de candidato"}), 200
 
-UPLOAD_FOLDER = os.path.join(os.getcwd(), 'uploads/cvs')
-ALLOWED_EXTENSIONS = {'pdf', 'doc', 'docx'}
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads/cvs")
+ALLOWED_EXTENSIONS = {"pdf", "doc", "docx"}
 candidato_bp.config = {"UPLOAD_FOLDER": UPLOAD_FOLDER}
 
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
+
 def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 @candidato_bp.route("/tiene-cv", methods=["GET"])
 @role_required(["candidato"])
 def tiene_cv():
     id_candidato = get_jwt_identity()
     cv = CV.query.filter_by(id_candidato=id_candidato).first()
-    
+
     if cv:
         return jsonify({"has_cv": True, "cv_url": cv.url_cv}), 200
     else:
         return jsonify({"has_cv": False}), 200
+
 
 @candidato_bp.route("/postularme", methods=["POST"])
 @role_required(["candidato"])
@@ -56,7 +68,7 @@ def postularme():
     cv = CV.query.filter_by(id=id_cv, id_candidato=id_candidato).first()
     if not cv:
         return jsonify({"error": "CV inválido o no pertenece al usuario"}), 403
-    
+
     oferta = Oferta_laboral.query.get(id_oferta)
     if not oferta:
         return jsonify({"error": "Oferta laboral no encontrada"}), 404
@@ -74,32 +86,39 @@ def postularme():
 
     return jsonify({"message": "Postulación realizada correctamente."}), 201
 
+
 @candidato_bp.route("/mis-cvs", methods=["GET"])
 @role_required(["candidato"])
 def listar_cvs():
     id_candidato = get_jwt_identity()
-    cvs = CV.query.filter_by(id_candidato=id_candidato).order_by(CV.fecha_subida.desc()).all()
+    cvs = (
+        CV.query.filter_by(id_candidato=id_candidato)
+        .order_by(CV.fecha_subida.desc())
+        .all()
+    )
 
-    return jsonify([
-        {
-            "id": cv.id,
-            "url": cv.url_cv,
-            "tipo_archivo": cv.tipo_archivo,
-            "fecha_subida": cv.fecha_subida.isoformat()
-        }
-        for cv in cvs
-    ]), 200
+    return jsonify(
+        [
+            {
+                "id": cv.id,
+                "url": cv.url_cv,
+                "tipo_archivo": cv.tipo_archivo,
+                "fecha_subida": cv.fecha_subida.isoformat(),
+            }
+            for cv in cvs
+        ]
+    ), 200
 
 
 @candidato_bp.route("/upload-cv", methods=["POST"])
 @role_required(["candidato"])
 def upload_cv():
-    if 'file' not in request.files:
+    if "file" not in request.files:
         return jsonify({"error": "No se encontró ningún archivo"}), 400
 
-    file = request.files['file']
+    file = request.files["file"]
 
-    if file.filename == '':
+    if file.filename == "":
         return jsonify({"error": "No se seleccionó ningún archivo"}), 400
 
     if file and allowed_file(file.filename):
@@ -110,7 +129,7 @@ def upload_cv():
         file.save(filepath)
 
         id_candidato = get_jwt_identity()
-        
+
         tipo_archivo = file.mimetype
 
         url_cv = f"/uploads/{filename}"
@@ -119,7 +138,7 @@ def upload_cv():
             id_candidato=id_candidato,
             url_cv=url_cv,
             tipo_archivo=tipo_archivo,
-            fecha_subida=datetime.now(timezone.utc)
+            fecha_subida=datetime.now(timezone.utc),
         )
 
         db.session.add(nuevo_cv)
@@ -128,6 +147,7 @@ def upload_cv():
         return jsonify({"message": "CV subido exitosamente", "file_path": url_cv}), 201
 
     return jsonify({"error": "Formato de archivo no permitido"}), 400
+
 
 @candidato_bp.route("/registrar-empresa", methods=["POST"])
 @role_required(["candidato"])
@@ -139,21 +159,25 @@ def registrar_empresa():
     cvv_tarjeta = data.get("card_cvv")
     tipo_tarjeta = data.get("card_type")
     nombre_empresa = data.get("company_name")
-    
+
     if not identifier:
         return jsonify({"error": "Debe ingresar username o email"}), 400
 
-    user = Usuario.query.filter((Usuario.username == identifier) | (Usuario.correo == identifier)).first()
+    user = Usuario.query.filter(
+        (Usuario.username == identifier) | (Usuario.correo == identifier)
+    ).first()
     if user:
         if not nombre_tarjeta or not numero_tarjeta or not cvv_tarjeta:
             return jsonify({"error": "Los datos de la tarjeta son obligatorios"}), 400
         if not nombre_empresa:
             return jsonify({"error": "El nombre de la empresa es obligatorio"}), 400
-        
+
         admin_emp_role = db.session.query(Rol).filter_by(slug="admin-emp").first()
         # Si no existe el rol admin-emp, crearlo
         if not admin_emp_role:
-            admin_emp_role = Rol(nombre="Admin-EMP", permisos="permisos_admin_emp", slug="admin-emp")
+            admin_emp_role = Rol(
+                nombre="Admin-EMP", permisos="permisos_admin_emp", slug="admin-emp"
+            )
             db.session.add(admin_emp_role)
             db.session.commit()
 
@@ -189,19 +213,17 @@ def registrar_empresa():
         return jsonify({"message": "Empresa registrada exitosamente"}), 201
     return jsonify({"error": "Usuario no encontrado"}), 404
 
+
 @candidato_bp.route("/empresas", methods=["GET"])
 @role_required(["candidato"])
 def obtener_empresas():
     empresas = Empresa.query.all()
     resultado = [
-        {
-            "id": empresa.id,
-            "nombre": empresa.nombre,
-            "correo": empresa.correo
-        }
+        {"id": empresa.id, "nombre": empresa.nombre, "correo": empresa.correo}
         for empresa in empresas
     ]
     return jsonify(resultado), 200
+
 
 @candidato_bp.route("/empresas/<string:nombre_empresa>/ofertas", methods=["GET"])
 @role_required(["candidato"])
@@ -211,8 +233,12 @@ def obtener_ofertas_por_nombre_empresa(nombre_empresa):
     if not empresa:
         return jsonify({"error": "Empresa no encontrada"}), 404
 
+    query = Oferta_laboral.query.filter_by(id_empresa=empresa.id)
+
+    query = construir_query_con_filtros(request, query)
+
     # Obtener las ofertas laborales asociadas a la empresa
-    ofertas = Oferta_laboral.query.filter_by(id_empresa=empresa.id).all()
+    ofertas = query.all()
     resultado = [
         {
             "id": oferta.id,
@@ -226,18 +252,20 @@ def obtener_ofertas_por_nombre_empresa(nombre_empresa):
             "currency": oferta.currency,
             "experience_level": oferta.experience_level,
             "fecha_publicacion": oferta.fecha_publicacion,
-            "fecha_cierre": oferta.fecha_cierre
+            "fecha_cierre": oferta.fecha_cierre,
         }
         for oferta in ofertas
     ]
-    return jsonify({
-        "empresa": {
-            "id": empresa.id,
-            "nombre": empresa.nombre,
-            "correo": empresa.correo
-        },
-        "ofertas": resultado
-    }), 200
+    return jsonify(
+        {
+            "empresa": {
+                "id": empresa.id,
+                "nombre": empresa.nombre,
+                "correo": empresa.correo,
+            },
+            "ofertas": resultado,
+        }
+    ), 200
 
 
 @candidato_bp.route("/recomendaciones", methods=["GET"])
@@ -246,7 +274,11 @@ def recomendar_ofertas():
     try:
         id_candidato = get_jwt_identity()
 
-        cv = CV.query.filter_by(id_candidato=id_candidato).order_by(CV.fecha_subida.desc()).first()
+        cv = (
+            CV.query.filter_by(id_candidato=id_candidato)
+            .order_by(CV.fecha_subida.desc())
+            .first()
+        )
         if not cv:
             return jsonify({"error": "El candidato no tiene un CV cargado"}), 400
 
@@ -268,13 +300,15 @@ def recomendar_ofertas():
             max_sim = max(cosine_similarity([vector_keywords], vectores_cv)[0])
             porcentaje = round(max_sim * 100, 2)
 
-            recomendaciones.append({
-                "id_oferta": oferta.id,
-                "nombre_oferta": oferta.nombre,
-                "empresa": oferta.empresa.nombre,
-                "coincidencia": porcentaje,
-                "palabras_clave": palabras_clave
-            })
+            recomendaciones.append(
+                {
+                    "id_oferta": oferta.id,
+                    "nombre_oferta": oferta.nombre,
+                    "empresa": oferta.empresa.nombre,
+                    "coincidencia": porcentaje,
+                    "palabras_clave": palabras_clave,
+                }
+            )
 
         recomendaciones.sort(key=lambda x: x["coincidencia"], reverse=True)
 
@@ -282,3 +316,47 @@ def recomendar_ofertas():
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+
+
+def construir_query_con_filtros(req, query):
+    # Extraccion de query params
+    nombre = req.args.get("nombre")
+    workplace_type = req.args.get("workplace_type")
+    employment_type = req.args.get("employment_type")
+    salary_min = req.args.get("salary_min", type=int)
+    salary_max = req.args.get("salary_max", type=int)
+    experience_level = req.args.get("experience_level")
+    location = req.args.get("location")
+    fecha_cierre = req.args.get("fecha_cierre")
+    fecha_publicacion = req.args.get("fecha_publicacion")
+
+    # Parse de fechas
+    try:
+        if fecha_publicacion:
+            fecha_publicacion = datetime.fromisoformat(fecha_publicacion)
+        if fecha_cierre:
+            fecha_cierre = datetime.fromisoformat(fecha_cierre)
+    except ValueError:
+        return jsonify({"error": "Formato de fecha inválido. Usa YYYY-MM-DD."}), 400
+
+    # Aplicar filtros en caso de que estén presentes
+    if nombre:
+        query = query.filter(Oferta_laboral.nombre.ilike(f"%{nombre}%"))
+    if salary_min is not None:
+        query = query.filter(Oferta_laboral.salary_min >= salary_min)
+    if salary_max is not None:
+        query = query.filter(Oferta_laboral.salary_max <= salary_max)
+    if employment_type is not None:
+        query = query.filter(Oferta_laboral.employment_type == employment_type)
+    if workplace_type is not None:
+        query = query.filter(Oferta_laboral.workplace_type == workplace_type)
+    if experience_level is not None:
+        query = query.filter(Oferta_laboral.experience_level == experience_level)
+    if location is not None:
+        query = query.filter(Oferta_laboral.location.ilike(f"%{location}%"))
+    if fecha_publicacion:
+        query = query.filter(Oferta_laboral.fecha_publicacion >= fecha_publicacion)
+    if fecha_cierre:
+        query = query.filter(Oferta_laboral.fecha_cierre <= fecha_cierre)
+
+    return query
