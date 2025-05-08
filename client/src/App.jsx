4d4bbc12a1e27427954125de0dc2ab7957f4a81c
Joaquin Tonizzo <joaquintonizzo1@gmail.com>
2025-05-08 19:24:51 -0300

    delete EmpleadoHome.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,6 @@ import LobbyEmp from "./pages/LobbyEmp";
 import Login from "./pages/Login";
 import ManagerHome from "./pages/ManagerHome";
 import Pagos from "./pages/Pagos";
-import EmpleadoHome from "./pages/EmpleadoHome"
 
 import { ReclutadorLayout } from "./components/ReclutadorLayout";
 import Clientes from "./pages/Clientes";
@@ -36,7 +35,6 @@ function App() {
         <Route path="/pagos" element={<Pagos />} />
         <Route path="/adminemp/home" element={<AdminEmpHome />} />
         <Route path="/manager/home" element={<ManagerHome />} />
-        <Route path="/empelado/home" element={<EmpleadoHome/>} />
       </Routes>
     </BrowserRouter>
   );
