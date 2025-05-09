1de1715bba11875f61a4b63ccc95cdd7143c4093
SantiagoZarate <santiago2001zarate@gmail.com>
2025-05-09 00:38:34 -0300

    agrega funciones nuevas a empleado service

--- client/src/services/empleadoService.js
+++ client/src/services/empleadoService.js
@@ -43,4 +43,67 @@ export const empleadoService = {
     const data = await fetcher({ url });
     return data;
   },
+  async subirCV({ file }) {
+    const url = `${API_URL}/api/upload-cv-empleado`;
+
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const options = {
+      method: 'POST',
+      body: formData,
+    }
+
+    const data = await fetcher({ url, options });
+    return data
+  },
+  async obtenerOfertasLaborales({
+    nombreEmpresa,
+    location,
+    workplaceType,
+    employmentType,
+    experienceLevel,
+    keywords,
+    salaryMin,
+    salaryMax
+  }) {
+    const queryParams = new URLSearchParams();
+
+    if (location) queryParams.append("location", location);
+    if (workplaceType) queryParams.append("workplace_type", workplaceType);
+    if (employmentType) queryParams.append("employment_type", employmentType);
+    if (experienceLevel) queryParams.append("experience_level", experienceLevel);
+    if (keywords) queryParams.append("keywords", keywords);
+    if (salaryMin !== undefined) queryParams.append("salary_min", salaryMin);
+    if (salaryMax !== undefined) queryParams.append("salary_max", salaryMax);
+
+    const url = `${API_URL}/api/empresas-empleado/${encodeURIComponent(nombreEmpresa)}/ofertas?${queryParams.toString()}`;
+
+    const data = await fetcher({ url });
+    return data;
+  },
+  async obtenerRecomendaciones() {
+    const url = `${API_URL}/api/recomendaciones-empleado`
+
+    const data = await fetcher({ url });
+    return data
+  },
+  async obtenerInfoEmpleado() {
+    const url = `${API_URL}/api/info-empleado`
+
+    const data = await fetcher({ url });
+    return data
+  },
+  async obtenerOfertasFiltradas({ sector, ubicacion, modalidad }) {
+    const query = new URLSearchParams();
+
+    if (sector) query.append("sector", sector);
+    if (ubicacion) query.append("ubicacion", ubicacion);
+    if (modalidad) query.append("modalidad", modalidad);
+
+    const url = `${API_URL}/api/ofertas-filtradas-empleado?${query.toString()}`;
+
+    const data = await fetcher({ url });
+    return data;
+  }
 }
