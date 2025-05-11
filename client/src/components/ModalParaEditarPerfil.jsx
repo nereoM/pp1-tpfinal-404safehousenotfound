4b34a2e9b61da2567b5558155313da5c83b96d55
NazaSoto19 <nazarenosotomayor19@gmail.com>
2025-05-11 11:37:13 -0300

    Cambio en modalPerfil

--- client/src/components/ModalParaEditarPerfil.jsx
+++ client/src/components/ModalParaEditarPerfil.jsx
@@ -60,7 +60,7 @@ export default function ModalParaEditarPerfil({
         <div className="w-full">
           <label className="flex items-center justify-center gap-2 p-2 border border-gray-300 rounded cursor-pointer bg-gray-100 hover:bg-gray-200 transition">
             <Upload className="w-5 h-5 text-gray-600" />
-            <span className="text-sm text-gray-700">Subir imagen</span>
+            <span className="text-sm text-gray-700">Nueva foto de perfil</span>
             <input
               type="file"
               accept="image/*"
