5156e2151b204ce2319d00dd4d931c95527eb877
Naim21 <2002natorres@gmail.com>
2025-05-14 00:18:31 -0300

    Riesgos laborales en main de generación

--- server/ml/desempeño&desarrollo/generation.py
+++ server/ml/desempeño&desarrollo/generation.py
@@ -1,6 +1,8 @@
-from dataset_generator import generate_employee_dataset, add_future_performance
+from dataset_generator import generate_employee_dataset, add_future_performance, add_risks
 
 if __name__ == "__main__":
     df_emps, emps_ruta = generate_employee_dataset(400)
 
     df_emps_rendFut, ruta_emps_rendFut = add_future_performance(emps_ruta)
+
+    df_emps_riesgos, ruta_emps_riesgos = add_risks(ruta_emps_rendFut)
