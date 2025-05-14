bf985909d61d16b812f833eac46a98b04d9060e0
Naim21 <2002natorres@gmail.com>
2025-05-13 23:56:53 -0300

    Rendimiento futuro en main de generación

--- server/ml/desempeño&desarrollo/generation.py
+++ server/ml/desempeño&desarrollo/generation.py
@@ -1,4 +1,6 @@
-from dataset_generator import generate_employee_dataset
+from dataset_generator import generate_employee_dataset, add_future_performance
 
 if __name__ == "__main__":
     df_emps, emps_ruta = generate_employee_dataset(400)
+
+    df_emps_rendFut, ruta_emps_rendFut = add_future_performance(emps_ruta)
