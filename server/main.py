7af3c6aa952f884372613aed52d81075bc2e748c
Nereo Manganiello <nereomanganiello@gmail.com>
2025-05-16 20:17:45 -0300

    .

--- server/main.py
+++ server/main.py
@@ -89,4 +89,5 @@ def iniciar_db():
             
 if __name__ == "__main__":
     iniciar_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
