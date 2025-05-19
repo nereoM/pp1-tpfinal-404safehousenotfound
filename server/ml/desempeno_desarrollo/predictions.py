34dec5ca2d2030ce39ddd6d88d4d944cfefce6ce
Naim21 <2002natorres@gmail.com>
2025-05-19 13:26:22 -0300

    Predicciones individuales de rendimiento y riesgos funcionales

--- server/ml/desempeno_desarrollo/predictions.py
+++ server/ml/desempeno_desarrollo/predictions.py
@@ -3,43 +3,6 @@ import joblib
 import os
 from sklearn.preprocessing import LabelEncoder
 
-"""
-def predecir_rend_futuro(ruta_csv, ruta_modelo='server/ml/trained_models/performance_model.pkl'):
-    try:
-        # Cargar los datos de empleados
-        df = pd.read_csv(ruta_csv)
-        
-        # Verificar columnas requeridas
-        columnas_requeridas = ['desempeno_previo', 'cantidad_proyectos', 'tamano_equipo',
-                            'horas_extras', 'antiguedad', 'horas_capacitacion']
-        
-        missing_cols = [col for col in columnas_requeridas if col not in df.columns]
-        if missing_cols:
-            print(f"Error: Faltan columnas requeridas: {missing_cols}")
-            return None
-        
-        # Cargar el modelo entrenado
-        modelo = joblib.load(ruta_modelo)
-        
-        # Preparar datos para predicción
-        X = df[columnas_requeridas]
-        
-        # Realizar predicciones
-        df['rendimiento_futuro_predicho'] = modelo.predict(X)
-        
-        # Guardar resultados
-        df.to_csv("server/ml/data/rendFut_predichos_use&predict.csv", index=False)
-
-        return df
-    
-    except FileNotFoundError as e:
-        print(f"Error: {e}")
-        return None
-    except Exception as e:
-        print(f"Error inesperado: {str(e)}")
-        return None
-"""
-
 def predecir_rend_futuro(df, ruta_modelo='server/ml/desempeno_desarrollo/trained_models/performance_model.pkl'):
     try:
         # current_directory = os.path.dirname(__file__)
