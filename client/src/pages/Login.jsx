902fa470f7475f5d7d2edbb2032b8417033a9615
NazaSoto19 <nazarenosotomayor19@gmail.com>
2025-05-23 12:22:02 -0300

    Prueba login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -290,7 +290,10 @@ export default function Login() {
       </button>
 
       {/* fondo */}
-      <div className="absolute inset-0 bg-[url('/city.jpg')] bg-cover bg-center blur-sm brightness-40 z-0"></div>
+      <div className="absolute inset-0 z-0">
+        <div className="w-full h-full bg-[url('/city_optimized.webp')] bg-cover bg-center" />
+        <div className="absolute inset-0 bg-black/40 backdrop-blur-sm" />
+      </div>
 
       {/* sobre nosotros */}
       <button onClick={() => setShowInfo(true)} className="absolute bottom-2 right-4 text-xs text-gray-500 hover:text-white z-20">
